6603411ec8eed623e91007d3d2c6c10332255b8b
Gururajan Srinivasan <Gururajan_S@ad.infosys.com>
2019-11-06 14:15:25 -0800

    added new line

--- vscode_1.py
+++ vscode_1.py
@@ -3,7 +3,7 @@ print("Hello World")
 # getting the value from the user
 name = input("Enter your name: ")
 # printing the new line
-print("Guru")
+print("Guru is checking on the GIT")
 # printing the next line
 print("Hello, \n Thanks for coming in ", name)
-
+print("new line")
