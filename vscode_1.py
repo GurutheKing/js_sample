2653cac33adce3d0f5f06c24bcc43474cd77f5da
Gururajan Srinivasan <Gururajan_S@ad.infosys.com>
2019-11-06 14:11:43 -0800

    Commit-test

---
+++ vscode_1.py
@@ -0,0 +1,9 @@
+# Basic python code for printing the statement
+print("Hello World")
+# getting the value from the user
+name = input("Enter your name: ")
+# printing the new line
+print()
+# printing the next line
+print("Hello, \n Thanks for coming in ", name)
+
