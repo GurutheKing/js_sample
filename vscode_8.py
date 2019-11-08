94e4b63e4cc2316a3f91dc545d151588066e96eb
Gururajan Srinivasan <Gururajan_S@ad.infosys.com>
2019-11-08 13:59:30 -0800

    vscode4 commitesd

---
+++ vscode_8.py
@@ -0,0 +1,26 @@
+course ="The values are \"good\""
+print(course)
+letter ='''
+Hello guru,
+thanks for coming
+'''
+
+print(letter)
+print(course[0:2])
+print(len(course))
+#print(firstname.upper())
+
+
+firstname="gururajan"
+lastname="Srinivasan"
+output =f'{firstname} [{lastname}] is a coder'
+output2 =firstname + " [" + lastname + "] is a coder"
+print(output)
+print(output2)
+print(firstname.capitalize())
+print(lastname.upper())
+print(lastname.lower())
+print(firstname.find('guru'))
+
+course3= "Python for beginners"
+print(len(course3))
