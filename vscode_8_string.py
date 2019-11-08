1c6c633c313c411944deecec97faf6f045ff33f0
gururajans <gururajan.s@outlook.com>
2019-11-08 00:06:20 -0800

    practice completed

--- vscode_8_string.py
+++ vscode_8_string.py
@@ -1,4 +1,8 @@
 firstname ="Gururajan"
 lastname="Srinivasan"
-output=f'{firstname} is teh first name and {lastname is the lastname}'
-
+output=f'{firstname} is the first name and {lastname} is the lastname'
+print(output.upper())
+print(output.lower())
+print(output.find('n'))
+print(output.replace('lastname', 'surname'))
+print('name' in output)
