7b84c765729a362b54d4a8a5248f92b45f676561
gururajans <gururajan.s@outlook.com>
2019-11-08 09:46:42 -0800

    added values validations

---
+++ vscode_11_values_validation.py
@@ -0,0 +1,6 @@
+temperature =97
+if temperature > 90 and temperature < 95:
+    print(f"the current temparature is {temperature} and its really good!!")
+else:
+    print(f"the current temparature is {temperature} and its getting hot!!")
+
