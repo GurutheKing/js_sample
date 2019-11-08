a7633913f51f60ff1b0938452ede93d3145d41dc
gururajans <gururajan.s@outlook.com>
2019-11-08 09:10:47 -0800

    code10added

---
+++ vscode_9_conditions.py
@@ -0,0 +1,16 @@
+# write a code to check whether th ewater is hot or cold
+#real scenario: the input is received fromt eh thermometert o intert via IOT and the value is fee, for now we use dummy value
+# house 2 million
+#if buyer has good credit - give down 10%
+#if buyer has bad credit - give down 20%
+# print down payment
+
+house_cost =1000000
+credit_good =True
+
+if credit_good:
+    down_payment =house_cost*0.1
+else:
+    down_payment =house_cost*0.1
+#print("Downpayment to be paid is " + str(down_payment))
+print(f"Downpayment to be paid is ${down_payment}")
