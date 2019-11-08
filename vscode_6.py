50dd0f2b94ec9396bf3ec297fde21ba96e20cee9
Gururajan Srinivasan <Gururajan_S@ad.infosys.com>
2019-11-07 16:08:24 -0800

    vscode

---
+++ vscode_6.py
@@ -0,0 +1,11 @@
+#name = "Gururajan Srinivasan"
+#phone = 2367777777
+#is_published = True
+#age = 25
+#print("We check in a patient named", + name + "He is " +
+ #     age + "years old and is a " + is_published + "patient")
+#Patient_type = "new"
+
+name = input("What is your name? ")
+color = input("What is the color you like? ")
+print("Hi " + name +", your fav color is " + color)
