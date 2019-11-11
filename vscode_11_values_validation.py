e578d47bfb0b6cc486c6757ff3c9f2a367856f84
gururajans <gururajan.s@outlook.com>
2019-11-10 16:24:28 -0800

    Vscode_12 -Games added

--- vscode_11_values_validation.py
+++ vscode_11_values_validation.py
@@ -1,6 +1,11 @@
-temperature =97
-if temperature > 90 and temperature < 95:
+temperature = int(input("Please ente the temperature: "))
+if temperature > 90 and temperature < 97:
     print(f"the current temparature is {temperature} and its really good!!")
+elif temperature == 90:
+    print(f"the current temparature is {temperature} and its about to get good!!")
+elif temperature >= 97:
+    print(f"the current temparature is {temperature} and its hot!!")
+
 else:
-    print(f"the current temparature is {temperature} and its getting hot!!")
+    print(f"the current temparature is {temperature} and its cold!!")
 
